f178b5880a3ed08ee25d04ebaec6f71040d4e09b
Duda Nunes <eduardapmn@gmail.com>
2023-11-22 00:28:38 -0300

    Initial commit

---
+++ Aula 2 - Variáveis e In Out/exercicio 1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int n,m ;
+    
+  cin >> n;
+  cin >> m;
+  cout << "Diferenca entre a pressao desejada e lida: " << n-m;
+  return 0;
+}
